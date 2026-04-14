9a5f9cc6ce7046bc511be1dcb765659a51790933
Nathan Smith - Wandsworth Sash Windows <nathan.smith@wandsworthsashwindows.com>
2026-04-14 11:39:36 +0100

    fix syntax error in leads

--- src/pages/Leads.jsx
+++ src/pages/Leads.jsx
@@ -459,7 +459,7 @@ export default function Leads() {
                         </button>
                       </td>
                     </tr>
-                  )}))}
+                  )})}
                 </tbody>
               </table>
             </div>
